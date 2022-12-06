0964549f87bc587c9bccd599c954a22817253e35
Navaneeth-pv <navaneethpv902@gmail.com>
2022-12-07 01:26:58 +0530

    first commit

---
+++ src/main/java/com/example/Patientbackend/PatientbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Patientbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientbackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/Patientbackend/controller/StudentController.java
@@ -0,0 +1,7 @@
+package com.example.Patientbackend.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+}
