bb5ea4ebce75603ad6151d4b206933b278aeacc5
Navaneeth-pv <navaneethpv902@gmail.com>
2022-12-07 07:45:24 +0530

    get and post added

--- src/main/java/com/example/Patientbackend/controller/StudentController.java
+++ src/main/java/com/example/Patientbackend/controller/StudentController.java
@@ -1,7 +1,19 @@
 package com.example.Patientbackend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
+
+    @PostMapping("/add")
+    public String AddPatient(){
+        return "Patient added successfully";
+    }
+
+    @GetMapping("/view")
+    public  String ViewAllPatient(){
+        return "view all ptient";
+    }
 }
