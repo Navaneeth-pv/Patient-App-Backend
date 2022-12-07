4b8ea6d99580c5ec78a593af9e85ce982271da43
Navaneeth-pv <navaneethpv902@gmail.com>
2022-12-07 08:27:49 +0530

    view patient added

--- src/main/java/com/example/Patientbackend/controller/StudentController.java
+++ src/main/java/com/example/Patientbackend/controller/StudentController.java
@@ -1,13 +1,19 @@
 package com.example.Patientbackend.controller;
 
+import com.example.Patientbackend.dao.PatientDao;
 import com.example.Patientbackend.model.Patients;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class StudentController {
+
+    @Autowired
+    private PatientDao dao;
+
+    @CrossOrigin(origins = "*")
 
     @PostMapping(path = "/add",consumes = "application/json",produces = "application/json")
     public String AddPatient(@RequestBody Patients p){
@@ -16,11 +22,16 @@ public class StudentController {
         System.out.println(p.getAddress().toString());
         System.out.println(p.getContactNo().toString());
         System.out.println(p.getDoctorName().toString());
+
+        dao.save(p);
         return "Patient added successfully";
     }
 
+    @CrossOrigin(origins = "*")
+
+
     @GetMapping("/view")
-    public  String ViewAllPatient(){
-        return "view all ptient";
+    public List<Patients> ViewAllPatient(){
+        return (List<Patients>) dao.findAll();
     }
 }
