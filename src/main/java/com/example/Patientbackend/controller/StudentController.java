bdeb5da4cfd8a4fb6263cde5222067a2aa8aac8f
Navaneeth-pv <navaneethpv902@gmail.com>
2022-12-07 08:06:04 +0530

    add patient

--- src/main/java/com/example/Patientbackend/controller/StudentController.java
+++ src/main/java/com/example/Patientbackend/controller/StudentController.java
@@ -1,14 +1,21 @@
 package com.example.Patientbackend.controller;
 
+import com.example.Patientbackend.model.Patients;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
 
-    @PostMapping("/add")
-    public String AddPatient(){
+    @PostMapping(path = "/add",consumes = "application/json",produces = "application/json")
+    public String AddPatient(@RequestBody Patients p){
+        System.out.println(p.getPatientName().toString());
+        System.out.println(p.getPatientId().toString());
+        System.out.println(p.getAddress().toString());
+        System.out.println(p.getContactNo().toString());
+        System.out.println(p.getDoctorName().toString());
         return "Patient added successfully";
     }
 
